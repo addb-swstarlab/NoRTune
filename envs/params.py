f7e4f309cc383d92df309ddab90263ed3f8edb86
dkdl012 <dkdl031@naver.com>
2024-02-07 15:33:22 +0900

    add variables.. envs/params.py

--- envs/params.py
+++ envs/params.py
@@ -7,6 +7,8 @@ PROJECT_NAME = 'SparkTuning'
 SPARK_CONF_INFO_CSV_PATH = os.path.join(HOME_PATH, PROJECT_NAME, 'data/Spark_3.1_45_parameters.csv')
 SPARK_CONF_PATH = os.path.join(HOME_PATH, PROJECT_NAME, 'data/add-spark.conf')
 
+DATA_FOLDER_PATH = os.path.join(HOME_PATH, PROJECT_NAME, 'data')
+
 MASTER_ADDRESS = GCP_SPARK_MASTER_ADDRESS
 MASTER_CONF_PATH = os.path.join(HOME_PATH, 'HiBench/conf')
 # MASTER_BENCH_BASH = os.path.join(HOME_PATH, 'scripts/run_terasort.sh')
