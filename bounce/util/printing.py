3f3f06cb06a3400fb97ede628453b3ec2949014a
dkdl012 <dkdl031@naver.com>
2024-03-04 23:34:31 +0900

    240304

--- bounce/util/printing.py
+++ bounce/util/printing.py
@@ -25,3 +25,23 @@ BOUNCE_NAME = """
     """
 
 BOUNCE_NAME = f"{BColors.LIGHTGREY}{BOUNCE_NAME}{BColors.ENDC}"
+
+RANDOM_NAME = """
+\t     ____                  __ 
+\t    / __ \____ _____  ____/ /___  ____ ___ 
+\t   / /_/ / __ `/ __ \\/ __  / __ \/ __ `__ \\
+\t  / _  _/ /_/ / / / / /_/ / /_/ / / / / / /
+\t /_/ |_|\__,_/_/ /_/\__,_/\____/_/ /_/ /_/ 
+
+  [ Random Search mode ]
+"""
+
+INCPP_NAME = """
+\t     __            ____  ____
+\t    /_/___  _____ / __ \\/ __ \\
+\t   / / __ \\/ ___// /_/ / /_/ /
+\t  / / / / / /__ / ____/ ____/
+\t /_/_/ /_/\\___//_/   /_/
+
+   [ incPP: Incresaingly high-dimensional combinatorial and continuous embedding with Pseudo Points ]
+"""
