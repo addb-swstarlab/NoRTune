93066bccf5170809b932e776e08bffee544905a7
dkdl012 <dkdl031@naver.com>
2024-03-04 23:35:06 +0900

    240304

--- main.py
+++ main.py
@@ -6,10 +6,11 @@ import os
 # import gin
 
 from bounce.bounce import Bounce
-from bounce.util.printing import BColors, BOUNCE_NAME
+from bounce.util.printing import BColors, BOUNCE_NAME, RANDOM_NAME, INCPP_NAME
 from bounce.spark_benchmark import SparkTuning
 from random_search.search import RandomSearch
 from random_search.benchmarks import SparkBench
+from incpp.incpp import incPP
 
 from envs.utils import get_logger
 from envs.spark import SparkEnv
@@ -20,19 +21,47 @@ logger = get_logger()
 os.system('clear')
 
 def main():
-    logging.basicConfig(
-        level=logging.INFO,
-        format=f"{BColors.LIGHTGREY} %(levelname)s:%(asctime)s - (%(filename)s:%(lineno)d) - %(message)s {BColors.ENDC}",
-    )
-
-    logging.info(BOUNCE_NAME)
-
-    then = time.time()
     parser = argparse.ArgumentParser(
         prog=BOUNCE_NAME,
         description="Bounce: Reliable High-Dimensional Bayesian Optimization Algorithm for Combinatorial and Mixed Spaces",
         epilog="For more information, please contact the author.",
     )
+    
+    parser.add_argument(
+        "--method",
+        type=str,
+        default='bounce',
+        help='bounce, random, ...'
+    )
+    parser.add_argument(
+        "--workload",
+        type=str,
+        choices=["aggregation", "join", "scan", "wordcount", "terasort", "bayes", "kmeans", "pagerank"],
+        default="join"
+    )
+    parser.add_argument(
+        "--neighbor",
+        type=float,
+        default=0.01
+    )
+
+    args = parser.parse_args()
+    
+    logging.basicConfig(
+        level=logging.INFO,
+        format=f"{BColors.LIGHTGREY} %(levelname)s:%(asctime)s - (%(filename)s:%(lineno)d) - %(message)s {BColors.ENDC}",
+    )
+
+    if args.method == 'bounce':
+        logging.info(BOUNCE_NAME)
+    elif args.method == 'random':
+        logging.info(RANDOM_NAME)
+    elif args.method == 'incpp':
+        logging.info(INCPP_NAME)
+            
+
+    then = time.time()
+
 
     # parser.add_argument(
     #     "--gin-files",
@@ -47,21 +76,7 @@ def main():
     #     nargs="+",
     #     default=[],
     # )
-    parser.add_argument(
-        "--method",
-        type=str,
-        default='bounce',
-        help='bounce, random, ...'
-    )
-    parser.add_argument(
-        "--workload",
-        type=str,
-        choices=["aggregation", "join", "scan", "wordcount", "terasort", "bayes", "kmeans", "pagerank"],
-        default="join"
-    )
 
-
-    args = parser.parse_args()
     print_params()
 
     # gin.parse_config_files_and_bindings(args.gin_files, args.gin_bindings)
@@ -74,6 +89,10 @@ def main():
         case "random":            
             benchmark = SparkBench(workload=args.workload)
             tuner = RandomSearch(benchmark=benchmark)
+        case "incpp":
+            env = SparkEnv(workload=args.workload)
+            benchmark = SparkTuning(env=env)
+            tuner = incPP(benchmark=benchmark, neighbor_distance=args.neighbor)
         case _:
             assert False, "The method is not defined.. Choose in [bounce, random]"
     
