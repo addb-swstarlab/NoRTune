d0957f6baf95961b9f6266ae708ee9b030e4a316
dkdl012 <dkdl031@naver.com>
2024-03-18 14:10:41 +0900

    adding to calculate improvement ratios from default res

--- incpp/incpp.py
+++ incpp/incpp.py
@@ -319,17 +319,18 @@ class incPP(Bounce):
                     self.trust_region.reset()
 
                     self.sample_init()
-        with lzma.open(os.path.join(self.results_dir, f"results.csv.xz"), "w") as f:
-            np.savetxt(
-                f,
-                np.hstack(
-                    (
-                        self.x_up_global.detach().cpu().numpy(),
-                        self.fx_global.detach().cpu().numpy().reshape(-1, 1),
-                    )
-                ),
-                delimiter=",",
-            )
+            with lzma.open(os.path.join(self.results_dir, f"results.csv.xz"), "w") as f:
+                np.savetxt(
+                    f,
+                    np.hstack(
+                        (
+                            self.x_up_global.detach().cpu().numpy(),
+                            self.fx_global.detach().cpu().numpy().reshape(-1, 1),
+                        )
+                    ),
+                    delimiter=",",
+                )
 
+        self.benchmark.env.calculate_improvement_from_default(best_fx=best_fx)
         
 
