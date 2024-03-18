48e3b87e05c3a79d4d3f11afada38340e8884d15
dkdl012 <dkdl031@naver.com>
2024-03-18 14:10:34 +0900

    adding to calculate improvement ratios from default res

--- envs/spark.py
+++ envs/spark.py
@@ -3,6 +3,7 @@ import logging
 import pandas as pd
 
 import envs.params as p
+from statistics import mean
 
 class SparkEnv:
     def __init__(
@@ -39,15 +40,20 @@ class SparkEnv:
         logging.info("Altering hibench workload scale..")
         logging.info(f"Workload ***{self.workload}*** need ***{workload_size[self.workload]}*** size..")
         os.system(f'scp {HIBENCH_CONF_PATH} {p.MASTER_ADDRESS}:{p.MASTER_CONF_PATH}/hibench.conf')
+
+
+    def apply_configuration(self, config_path=None):
+        config_path = self.config_path if config_path is None else config_path
         
-    def apply_and_run_configuration(self):
+        logging.info("Applying created configuration to the remote Spark server.. 💨💨")
+        os.system(f'scp {config_path} {p.MASTER_ADDRESS}:{p.MASTER_CONF_PATH}')
+        
+    def run_configuration(self):
         """
             TODO:
             !!A function to Save configuration should be implemented on other files!!
         """
-        logging.info("Applying created configuration to the remote Spark server.. 💨💨")
-        os.system(f'scp {self.config_path} {p.MASTER_ADDRESS}:{p.MASTER_CONF_PATH}')
-
+        
         exit_code = os.system(f'ssh {p.MASTER_ADDRESS} "bash --noprofile --norc -c scripts/run_{self.workload}.sh"')
         # exit_code = os.system(f'ssh {p.MASTER_ADDRESS} "bash --noprofile --norc -c scripts/run_bayes.sh"')
         if exit_code > 0:
@@ -82,4 +88,25 @@ class SparkEnv:
             logging.warning("💀Failed cleaning Spark Storage!!")
         else:
             logging.info("🎉Successfully cleaning Spark Storage")
+    
+
+    # Get the result of default configuration..
+    def _get_result_from_default_configuration(self) -> float: 
+        self.apply_configuration(config_path=p.SPARK_DEFAULT_CONF_PATH)
+        
+        res_ = []
+        for _ in range(p.BENCHMARKING_REPETITION):
+            self.run_configuration()
+            res_.append(self.get_results())
             
+        def_res = mean(res_)
+
+        return def_res
+    
+    def calculate_improvement_from_default(self, best_fx):
+        default_fx = self._get_result_from_default_configuration()
+        improve_ratio = round((default_fx - best_fx)/default_fx * 100, 2)
+        logging.info("=============================================================")
+        logging.info(f"🎯 Improvement rate from default results.. {improve_ratio}%")
+        logging.info(f"Default result is {default_fx} and Best result is {best_fx}")
+        logging.info("=============================================================")
