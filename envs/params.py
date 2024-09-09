84409c5a2def2664d88a80c34a386e1bf8ced211
dkdl012 <dkdl031@naver.com>
2024-09-09 13:26:15 +0900

    modify get project name

--- envs/params.py
+++ envs/params.py
@@ -2,7 +2,7 @@ import os
 from envs.gcp_info import GCP_SPARK_MASTER_ADDRESS, GCP_DATAPROC_STOP_COMMAND, GCP_DATAPROC_START_COMMAND
 
 HOME_PATH = os.path.expanduser('~')
-PROJECT_NAME = 'SparkTuning'
+PROJECT_NAME = os.path.split(os.getcwd())[-1]
 
 SPARK_CONF_INFO_CSV_PATH = os.path.join(HOME_PATH, PROJECT_NAME, 'data/Spark_3.1_45_parameters.csv')
 SPARK_CONF_PATH = os.path.join(HOME_PATH, PROJECT_NAME, 'data/add-spark.conf')
