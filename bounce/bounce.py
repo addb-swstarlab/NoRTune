c5e114bc4ffecd737f5ec27c4dfdc6cc1b567b94
dkdl012 <dkdl031@naver.com>
2024-12-30 15:38:53 +0900

    241114

--- bounce/bounce.py
+++ bounce/bounce.py
@@ -726,7 +726,7 @@ class Bounce:
                     delimiter=",",
                 )
         
-        self.benchmark.env.calculate_improvement_from_default(best_fx=best_fx)
+        # self.benchmark.env.calculate_improvement_from_default(best_fx=best_fx)
 
     @property
     def _forecasted_tr_dim(self) -> int:
