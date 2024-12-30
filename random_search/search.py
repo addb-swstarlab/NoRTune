91c44dd0bd3704df1231ceaf9578274c4ebb549e
dkdl012 <dkdl031@naver.com>
2024-12-30 15:42:51 +0900

    Update search.py for random

--- random_search/search.py
+++ random_search/search.py
@@ -1,24 +1,28 @@
 import os
 import json
 import logging
+import numpy as np
 from statistics import mean, stdev
+from typing import Union
 # from random_search.benchmarks import SparkBench
 from envs.utils import get_foldername
-from envs.params import BOUNCE_PARAM as bp, BENCHMARKING_REPETITION
-from random_search.benchmarks import SparkBench
+from envs.params import BOUNCE_PARAM as bp, BENCHMARKING_REPETITION, CONF_PATH, HOME_PATH, PROJECT_NAME
+from random_search.benchmarks import SparkBench, PostgresBench
 
 class RandomSearch: # Random Optimizer?
     def __init__(
         self,
-        benchmark : SparkBench,
-        maximum_number_evaluations: int = bp["maximum_number_evaluations"] # 100
+        benchmark : Union[SparkBench, PostgresBench],
+        maximum_number_evaluations: int = bp["maximum_number_evaluations"], # 100,
+        is_tps : bool = False
     ):
         # self.benchmark = benchmark if benchmark is not None else SparkBench()
         self.benchmark = benchmark
         self.maximum_number_evaluations = maximum_number_evaluations                        
         self._set_result_dir()
+        self.is_tps = is_tps
     
-    def _set_result_dir(self, path='/home/jieun/SparkTuning/random_search/results'):
+    def _set_result_dir(self, path=os.path.join(HOME_PATH, PROJECT_NAME, 'random_search/results')):
         self.results_dir = get_foldername(path)
         os.makedirs(self.results_dir, exist_ok=True)
         logging.info(f"Results are saved in .. {self.results_dir}")
@@ -40,12 +44,22 @@ class RandomSearch: # Random Optimizer?
         for i in range(self.maximum_number_evaluations):
             sampled_config = self.benchmark.random_sampling_configuration()
             self.benchmark.save_configuration_file(sampled_config)
+            sampled_config = dict(sampled_config)
+            sampled_config = {k: int(v) if isinstance(v, np.integer) else v for k, v in sampled_config.items()}
             
             res_ = []
+            self.benchmark.apply_configuration()
+            load = True
             for _ in range(BENCHMARKING_REPETITION):
-                self.benchmark.apply_and_run_configuration(load=True if _ == 0 else False)
-                res_.append(self.benchmark.get_results()) 
-                repeated_configs.append(sampled_config.get_dictionary())
+                self.benchmark.run_configuration(load)
+                load = False
+                # self.benchmark.apply_and_run_configuration(load=True if _ == 0 else False)
+                if self.is_tps:
+                    res_.append(-self.benchmark.get_results()) # Higher is better, tps
+                else:
+                    res_.append(self.benchmark.get_results()) # Lower is better, latency or runtime
+                # repeated_configs.append(dict(sampled_config))
+                repeated_configs.append(sampled_config)
                 repeated_results.append(res_)
             res = mean(res_)
            
@@ -56,11 +70,12 @@ class RandomSearch: # Random Optimizer?
                 best_res = res
                 
                 # best_config = sampled_config
-                f = open('/home/jieun/SparkTuning/data/add-spark.conf', 'r')
-                best_config = f.readlines()
-                best_config_dict = sampled_config
-                
-            configs.append(sampled_config.get_dictionary())
+                f = open(CONF_PATH, 'r')
+                best_config = f.readlines() 
+                best_config_dict = sampled_config 
+            
+            # configs.append(sampled_config.get_dictionary())
+            configs.append(sampled_config)
             results.append(res)
         
         # Save history.. configs and results
@@ -89,8 +104,12 @@ class RandomSearch: # Random Optimizer?
         logging.info(f"✨✨✨ Evaluating best x... # of repetitions = {BENCHMARKING_REPETITION} ✨✨✨")
         best_ys = []
         self.benchmark.save_configuration_file(best_config_dict)
+        self.benchmark.apply_configuration()
+        load = True
         for _ in range(BENCHMARKING_REPETITION):
-            self.benchmark.apply_and_run_configuration(load=True if _ == 0 else False)
+            self.benchmark.run_configuration(load)
+            load = False
+            # self.benchmark.apply_and_run_configuration(load=True if _ == 0 else False)
             best_ys.append(self.benchmark.get_results()) 
         logging.info(f"Results = {best_ys} , Mean = {mean(best_ys):.3f} (±{stdev(best_ys):.3f})")         
         # self.benchmark.calculate_improvement_from_default(best_res)    
